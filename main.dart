3d747c94ec4729227bee285b38e1585cd50e51b3
anshumanpanda <anshumanpanda711@gmail.com>
2020-05-08 16:09:26 +0100

    My first commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './app_screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Exploring UI Widgets",
+       home: Home(),
+  )); // MaterialApp
+}
